45d301a2432a1f1374ab648be85d50b17b312611
anita <hutagaola@gmail.com>
2016-07-21 17:42:06 +0800

    versi 1.0.0

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,22 +1,19 @@
 /*
- * TicTacToe.java
- *
- * This class is the main class that runs the application
- * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package tictactoe;
 
 /**
  *
- * @author Masood Fallahpoor , Mohammad Zebardast
+ * @author zxcvbn
  */
 public class Main
 {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) 
     {
 
